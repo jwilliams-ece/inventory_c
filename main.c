60121d9e908f92eb6c03fbec1abafbc6b177fa60
/ <thecwme@gmail.com>
2026-07-01 19:44:54 -0500

    Prototype + ignore file

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 // Main file to run inventory system
+#include <stdio.h>
+
+
+char *message = "Inventory open!";
+char *foodItem[] = {"banana", "apple", "bread", "meat"};
+
+int main()
+{
+    int len = sizeof(foodItem) / sizeof(foodItem[0]);
+    
+    for(int i = 0; i < len; i++) {
+        printf("%s\n",foodItem[i]);
+    }
+
+}
