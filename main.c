1a46e597b5d640d9cbf72ee75e42b99bef832600
/ <thecwme@gmail.com>
2026-07-02 11:39:29 -0500

    rudimentary addItem functionality

--- main.c
+++ main.c
@@ -1,16 +1,41 @@
 // Main file to run inventory system
 #include <stdio.h>
-
+#include <string.h>
 
 char *message = "Inventory open!";
-char *foodItem[5] = {"banana", "apple", "bread", "meat"};
+char *foodItem[6] = {"banana", "apple", "bread", "meat"};
+char item[] = "food";
+char item_2[] = "food_2";
 
-int main()
-{
+int itemCount(){
+
+    int count = 0;
+
+    while (foodItem[count] != NULL)
+    {
+        count++;
+    }
+    return count;
+}
+
+void addItem(char item[]) {
     int count = 0;
     
     while(foodItem[count] != NULL) {
-        printf("%s\n",foodItem[count]);
+        
         count++;
+    }
+    foodItem[count] = item;
+}
+
+int main()
+{
+    addItem(item);
+    addItem(item_2);
+
+    int len = sizeof(foodItem) / sizeof(foodItem[0]);
+
+    for (int i = 0; i < len - 1; i++) {
+        printf("%s\n",foodItem[i]);
     }
 }
