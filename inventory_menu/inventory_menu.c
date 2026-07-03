03d4ab48626a5fe1274fbe882fff4a86a1db1e7e
/ <thecwme@gmail.com>
2026-07-03 17:26:56 -0500

    modified printItems()

--- inventory_menu/inventory_menu.c
+++ inventory_menu/inventory_menu.c
@@ -9,12 +9,30 @@
 typedef struct
 {
     char *name;
-    int cost;
+    float cost;
 
 } Item;
 
-Item food_1 = {"Apple", 1};
+Item foodList[] = {
+    {"Apple", 1},
+    {"Banana", .50},
+    {"Orange", .75},
+    {"egg", 5},
+};
 
+/* loop function that displays all the current items in inventory*/
+
+void printItems() {
+    int len = sizeof(foodList) / sizeof(foodList[0]);
+
+    printf("%-15s %8s\n", "Name", "Cost");
+
+    for (int i = 0; i < len; i++) {
+        printf("%-15s $%7.2f\n", 
+                  foodList[i].name, 
+                 foodList[i].cost);
+    }
+}
 
 int inventoryMenu() {
 
@@ -22,7 +40,7 @@ int inventoryMenu() {
 
     printf("====INVENTORY MENU=================\r\n\r\n\r\n");
     printf("This is the current inventory      \n\n\n");
-    printf("Name: %s            \n\n\n", food_1.name);
+    printItems();
     printf("===================================\n\n");
 
     printf("Continue: 1\nExit: 0\n\n");
@@ -44,8 +62,6 @@ int inventoryMenu() {
         printf("Invalid Input.\n");
         break;
     }
-
-    printf("Program continues...\n");
 }
 
 
