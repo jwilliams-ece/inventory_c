e12d018e259969e3937206249ebe71e89e3ad019
/ <thecwme@gmail.com>
2026-07-03 10:54:00 -0500

    base layout for main menu

--- title_screen.c
+++ title_screen.c
@@ -1,8 +1,28 @@
-/* This will function as the title/menu screen for the program*/
+/* This will function as the title/menu screen for the program */
 
 #include <stdio.h>
 
+int input;
+
 int main() {
-    printf("Hello, World!");
-    return 0;
+    printf("====MAIN MENU======================\n\n\n");
+    printf("This is an inventory storage system\n\n\n");
+    printf("===================================\n\n");
+
+    printf("Continue: 1\nExit: 0\n\n");
+
+    printf("Input: ");
+    scanf("%d", input);
+
+    switch (input)
+    {
+    case 0:
+        /* code */
+        break;
+    case 1:
+        break;
+    
+    default:
+        break;
+    }
 }
